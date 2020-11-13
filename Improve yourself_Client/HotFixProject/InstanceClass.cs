26ebb4582f02c3bb46a9e572c8292e8c4c4a392f
ningwei179 <31332665+ningwei179@users.noreply.github.com>
2020-11-13 20:10:06 +0800

    V2.8.1
    添加了ILRuntime的demo

--- Improve yourself_Client/HotFixProject/InstanceClass.cs
+++ Improve yourself_Client/HotFixProject/InstanceClass.cs
@@ -13,5 +13,15 @@ namespace HotFixProject
         {
             UnityEngine.Debug.Log("!!! InstanceClass.StaticFunTest()");
         }
+
+        public static void StaticFunTest2(int num)
+        {
+            UnityEngine.Debug.Log("一个参数方法的调用"+num);
+        }
+
+        //public static void StaticFunTest2(int num1,int num2)
+        //{
+        //    UnityEngine.Debug.Log("2个参数方法的调用:param1=" + num1+"==param2="+num2);
+        //}
     }
 }
