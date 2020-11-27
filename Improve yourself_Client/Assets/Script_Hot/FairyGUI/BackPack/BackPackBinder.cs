ac62ce28c5c858f8c7e5b76cdeedcc2f72b41056
ningwei179 <31332665+ningwei179@users.noreply.github.com>
2020-11-27 17:26:43 +0800

    V5.0
    添加了FairyGUI

---
+++ Improve yourself_Client/Assets/Script_Hot/FairyGUI/BackPack/BackPackBinder.cs
@@ -0,0 +1,14 @@
+/** This is an automatically generated class by FairyGUI. Please do not modify it. **/
+
+using FairyGUI;
+
+namespace BackPack
+{
+    public class BackPackBinder
+    {
+        public static void BindAll()
+        {
+            UIObjectFactory.SetPackageItemExtension(UI_BackPackPanel.URL, typeof(UI_BackPackPanel));
+        }
+    }
+}
